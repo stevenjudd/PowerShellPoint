49c0af9d36c7dc86be677a866acf89bb6c6b2ea2
Steven Judd <steven.judd@gmail.com>
2020-03-31 00:56:06 -0500

    added source reference

--- SoundFun.ps1
+++ SoundFun.ps1
@@ -1,3 +1,5 @@
+#uses the nircmd.exe tool from https://www.nirsoft.net/utils/nircmd.html
+
 $SoundFun = Join-Path -Path $env:TEMP -ChildPath "soundfun.exe"
 Convert-TextToBinary -Text (Get-Content .\test.txt) -OutputPath $SoundFun
 Invoke-Expression "$SoundFun mutesysvolume 0"
